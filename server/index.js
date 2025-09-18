7db549a23681deb50e40d5668723cf03fbc4cf1c
alammuntasir <alammuntasir01@gmail.com>
2025-09-19 05:24:54 +0600

    route define successfull

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 require("dotenv").config();
+
+// database conntection
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err.message || err);
+  });
+
+//   route define
+
+app.post("/addtodo", (req, res) => {
+  return res
+    .status(201)
+    .json({ success: true, message: "todo created successfully" });
+});
+
+app.get("/gettodo", (req, res) => {
+  return res
+    .status(200)
+    .json({ success: true, message: "todo fetch successfully" });
+}); 
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running port number ${process.env.PORT}`);
